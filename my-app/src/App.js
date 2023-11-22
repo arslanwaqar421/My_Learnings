ad47ce55c0c95e65a697d06551a06f6b8bac3145
Arslan Waqar <arslanwaqar421@gmail.com>
2023-11-22 11:59:48 +0500

    Added full dark mode

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,27 @@ import Navbar from './components/Navbar';
 import './App.css';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import React,  { useState } from 'react';
 
 function App() {
+  const [Mode, setMode] = useState('light')
+
+  const toggleMode = () =>{
+    if(Mode === "light"){
+      setMode("dark")
+      document.body.style.backgroundColor= "grey"
+    }
+    else{
+      setMode("light")
+      document.body.style.backgroundColor="white"
+    }
+  }
   return (
     <>
-      {/* <Navbar title="Sitebar"  about="About Us"/>
+      <Navbar title="Sitebar"  about="About Us" mode={Mode} toggleMode={toggleMode}/>
       <div className="container my-4">
-      <TextForm heading="ExampleTextBox"/>
-      </div> */}
+      <TextForm heading="ExampleTextBox" mode ={Mode}/>
+      </div>
       <About/>
     </>
   );
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -19,19 +19,20 @@ export default function TextForm(props) {
         setText(Text.toLowerCase())
     }
 
+
     return (
         <>
-            <form id="form1">
+            <form id="form1" style={{color :props.mode === "light"?"black":"white"}}>
                 <h1>{props.heading}</h1>
-                <div className="mb-3">
-                    <textarea name="ex" id="text-1" cols="150" rows="13" value = {Text} onChange={handleOnChange}></textarea>
+                <div className="mb-3" style={{border : props.mode === "dark"? "1px solid white" : "1px solid black"}}>
+                    <textarea name="ex" style={{backgroundColor: props.mode === "light"?"white":"gray", color : props.mode === "light"?"black":"white"}} id="text-1" cols="150" rows="13" value = {Text} onChange={handleOnChange}></textarea>
                 </div>
                 <button className="btn btn-primary" onClick={handleClick}>Convert to Upper</button>
                 <button className="btn btn-primary mx-2" onClick={convertToLower}>Convert to Lower</button>
   
             </form>
-            <div className="container my-2">
-                <h1>Your text Summary</h1>
+            <div className="container my-2" >
+                <h1 >Your text Summary</h1>
                 <p>{Text.split(" ").length} words and {Text.length} Characters</p>
             </div>
         </>
