e4f86c16cef7e069a4efa601f0aaacf2845353a3
Arslan Waqar <arslanwaqar421@gmail.com>
2023-11-24 22:42:34 +0500

    Client Side Routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,43 +4,52 @@ import { useState } from 'react';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import Alert from './components/Alert';
+// Update the import statements for v6
+import {
+  BrowserRouter,
+  Routes, // Replace 'Switch' with 'Routes'
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   const [Mode, setMode] = useState('light')
-  const [alert,setAlert] = useState(null)
+  const [alert, setAlert] = useState(null)
 
-  const showAlert = (message,type)=> {
+  const showAlert = (message, type) => {
     console.log("in alert")
     setAlert({
-      msg : message,
-      typ:type
+      msg: message,
+      typ: type
     })
-    setTimeout(() => {setAlert(null)},2000)
+    setTimeout(() => { setAlert(null) }, 2000)
   }
 
-  const toggleMode = () =>{
-    if(Mode === "light"){
+  const toggleMode = () => {
+    if (Mode === "light") {
       setMode("dark")
-      document.body.style.backgroundColor= "grey"
+      document.body.style.backgroundColor = "grey"
       showAlert("Dark mode activated!", "success")
     }
-    else{
+    else {
       setMode("light")
-      document.body.style.backgroundColor="white"
-      showAlert("Light mode activated!","warning")
+      document.body.style.backgroundColor = "white"
+      showAlert("Light mode activated!", "warning")
     }
   }
 
 
   return (
-    <>
-      <Navbar title="Sitebar"  about="About Us" mode={Mode} toggleMode={toggleMode}/>
-      <Alert Alert = {alert}/>
-      <div className="container my-4">
-      <TextForm heading="ExampleTextBox" mode ={Mode}/>
-      </div>
-      <About/>
-    </>
+    <BrowserRouter>
+      <Navbar title="Sitebar" about="About Us" mode={Mode} toggleMode={toggleMode} />
+      <Alert Alert={alert} />
+        <div className="container my-4">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm heading="ExampleTextBox" mode={Mode} />} />
+          </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
 
